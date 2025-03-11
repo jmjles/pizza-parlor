8fdda8b8f39b35723eb0e32bf51da3bf7d041f99
jmjles <jmjles@gmail.com>
2025-03-10 19:12:55 -0700

    devop: added modal component

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,31 @@
+import { Container, Dialog, DialogContent, DialogTitle, IconButton, Typography as Font } from '@mui/material'
+import { Close } from '@mui/icons-material'
+import { JSX } from 'react'
+
+const Modal = (props: ModalType) => {
+
+    return (
+        <Dialog open={props.open} maxWidth="lg" onClose={props.handleClose}>
+            <Container style={{ textAlign: 'right' }} disableGutters>
+                <IconButton color="error"><Close /></IconButton>
+            </Container>
+            <DialogTitle align="center" variant="h3">
+                    {props.title}
+            </DialogTitle>
+            <DialogContent>
+                {props.children}
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+interface ModalType extends ModalPropType {
+    title: string;
+    children: JSX.Element;
+}
+
+export type ModalPropType = {
+    open: boolean;
+    handleClose: any;
+}
+export default Modal
