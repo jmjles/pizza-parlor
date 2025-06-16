5d89527595af100bdc636017422298f64e4972df
jmjles <jmjles@gmail.com>
2025-06-16 16:44:53 -0700

    Devop: Added menu

--- src/components/bar/Bar.tsx
+++ src/components/bar/Bar.tsx
@@ -1,16 +1,24 @@
 'use client'
-import { Menu, LocalPizza, Settings, ShoppingCart } from '@mui/icons-material'
+import { Menu, LocalPizza, Settings, ShoppingCart, Logout, ShoppingBag } from '@mui/icons-material'
 import {
     AppBar, Button,
     Container,
     Grid2,
     IconButton, Stack,
     Typography as Font,
+    Menu as MUIMenu, MenuItem, Avatar,
 } from '@mui/material'
 import blackBoard from '@/assets/images/blackboard.jpg'
 import woodBorder from '@/assets/images/wood-border.jpg'
+import { useRef, useState } from 'react'
 
 const Bar = (props: BarType) => {
+    const anchorEl = useRef(null)
+    const [open, setOpen] = useState(false)
+
+    const handleMenu = () => {
+        setOpen(p => !p)
+    }
     return (
         <AppBar position="sticky" style={{
             backgroundImage: `url(${blackBoard.src})`,
@@ -20,7 +28,7 @@ const Bar = (props: BarType) => {
             borderBottomStyle: 'solid',
         }}>
             <Container>
-                <Grid2 container justifyContent="space-between" wrap="nowrap">
+                <Grid2 container ref={anchorEl} justifyContent="space-between" wrap="nowrap">
                     <Button color="inherit" onClick={props.openLocations}>
                         <Grid2
                             container
@@ -45,28 +53,54 @@ const Bar = (props: BarType) => {
                     <Grid2 container spacing={2} wrap="nowrap" alignItems="center">
                         <Grid2 display={{ xs: 'none', sm: 'initial', xl: 'none' }}>
                             <IconButton color="inherit">
-                                <ShoppingCart fontSize="medium" />
+                                <ShoppingBag fontSize="medium" />
                             </IconButton>
                         </Grid2>
-                        <Grid2 display={{ xs: 'initial', xl: 'none' }}>
+                        <Grid2 display={{ xs: 'initial', xl: 'none' }}  onClick={handleMenu}>
                             <IconButton color="inherit">
                                 <Menu fontSize="medium" />
                             </IconButton>
                         </Grid2>
                         <Grid2 display={{ xs: 'none', xl: 'initial' }}>
                             <IconButton color="inherit">
-                                <Settings fontSize="medium" />
+                                <Settings fontSize="medium" onClick={handleMenu} />
                             </IconButton>
                         </Grid2>
                     </Grid2>
                 </Grid2>
+                <MUIMenu keepMounted anchorEl={anchorEl.current} open={open} onClose={handleMenu}
+                         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }} transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }} anchorPosition={{ top: 50, left: 0 }}>
+                    <MenuItem>
+                        <Stack direction="row" alignItems="center" spacing={2}>
+                            <Avatar />
+                            <Font>Username</Font>
+                        </Stack>
+                    </MenuItem>
+                    <MenuItem>
+                        <Stack direction="row" alignItems="center" spacing={2}>
+                            <ShoppingCart fontSize="large" />
+                            <Font>My Orders</Font>
+                        </Stack>
+                    </MenuItem>
+                    <MenuItem>
+                        <Stack direction="row" alignItems="center" spacing={2}>
+                            <Logout fontSize="large" />
+                            <Font>Log out</Font>
+                        </Stack>
+                    </MenuItem>
+                </MUIMenu>
             </Container>
         </AppBar>
     )
 }
+
 export type BarType = {
     city: string;
     location: string
     openLocations: any
 };
+
 export default Bar
